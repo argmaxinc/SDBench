afbfc0b80f3e843677d5e69190ee8634537cdd08
Eduardo Pacheco <eduardo@argmaxinc.com>
2025-07-05 17:29:05 -0300

    fix: pipeline __init__

--- src/sdbench/pipeline/__init__.py
+++ src/sdbench/pipeline/__init__.py
@@ -1,7 +1,8 @@
 # For licensing see accompanying LICENSE.md file.
 # Copyright (C) 2025 Argmax, Inc. All Rights Reserved.
 
-from .base import PIPELINE_REGISTRY, Pipeline, PipelineType, register_pipeline
+from .base import PIPELINE_REGISTRY, Pipeline, register_pipeline
+from .utils import PipelineType
 from .diarization import *
 from .orchestration import *
 from .streaming_transcription import *
