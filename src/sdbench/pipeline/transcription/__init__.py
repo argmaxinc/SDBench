d8f129e24abe59f57bb062b18fd893334082ed8d
Eduardo Pacheco <eduardo@argmaxinc.com>
2025-07-18 18:10:23 -0300

    Add new transcription pipeline and datasets

--- src/sdbench/pipeline/transcription/__init__.py
+++ src/sdbench/pipeline/transcription/__init__.py
@@ -2,3 +2,14 @@
 # Copyright (C) 2025 Argmax, Inc. All Rights Reserved.
 
 from .common import TranscriptionOutput
+from .speech_analyzer import SpeechAnalyzerConfig, SpeechAnalyzerPipeline
+from .whisperkit import WhisperKitPipeline, WhisperKitPipelineConfig
+
+
+__all__ = [
+    "TranscriptionOutput",
+    "SpeechAnalyzerPipeline",
+    "SpeechAnalyzerConfig",
+    "WhisperKitPipeline",
+    "WhisperKitPipelineConfig",
+]
